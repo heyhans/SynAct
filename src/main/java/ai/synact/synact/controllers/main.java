b2a1ad33744e55d57a25998831694709d604a13b
Hans <hans@hanspm.com>
2025-12-13 11:42:38 +0900

    Fixed links

--- src/main/java/ai/synact/synact/controllers/main.java
+++ src/main/java/ai/synact/synact/controllers/main.java
@@ -1,7 +1,9 @@
 package ai.synact.synact.controllers;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,43 +17,51 @@ public class main {
         return "index";
     }
 
-    @GetMapping("/about-us")
-    public String aboutUs() {
+    @GetMapping("/about")
+    public String aboutUs(Model model) {
+        model.addAttribute("activeMenu", "about");
         return "/main/about-us";
     }
 
     @GetMapping("/blog")
-    public String blog() {
+    public String blog(Model model) {
+        model.addAttribute("activeMenu", "blog");
         return "/main/blog";
     }
 
     @GetMapping("/blog-detail")
-    public String blogDetail() {
+    public String blogDetail(Model model) {
+        model.addAttribute("activeMenu", "blog");
         return "/main/blog-detail";
     }
 
     @GetMapping("/contact")
-    public String contact() {
+    public String contact(Model model) {
+        model.addAttribute("activeMenu", "contact");
         return "/main/contact";
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("activeMenu", "home");
         return "/main/home";
     }
 
     @GetMapping("/portfolio")
-    public String portfolio() {
+    public String portfolio(Model model) {
+        model.addAttribute("activeMenu", "portfolio");
         return "/main/portfolio";
     }
 
     @GetMapping("/portfolio-detail")
-    public String portfolioDetail() {
+    public String portfolioDetail(Model model) {
+        model.addAttribute("activeMenu", "portfolio");
         return "/main/portfolio-detail";
     }
 
     @GetMapping("/services")
-    public String services() {
+    public String services(Model model) {
+        model.addAttribute("activeMenu", "services");
         return "/main/services";
     }
 }
